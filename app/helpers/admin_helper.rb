8028ef0936a68851bef5ae53a7a0d038299acd3b
Marcio Trindade <marciotrindade@gmail.com>
2011-07-10 13:48:37 -0300

    changed admin helper to show title

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -3,10 +3,10 @@ module AdminHelper
 
   def data_table(options={})
     options.reverse_merge! "sPaginationType" => "full_numbers",
-                          "bStateSave" => true,
-                          "bInfo" => false,
-                          "iDisplayLength" => "100",
-                          "aaSorting" => [[ 0, "asc"]]
+                           "bStateSave" => true,
+                           "bInfo" => false,
+                           "iDisplayLength" => "100",
+                           "aaSorting" => [[ 0, "asc"]]
     content_for(:js) do
       javascript_tag do
         "$('.table').dataTable(#{options.to_json});".html_safe
@@ -14,22 +14,18 @@ module AdminHelper
     end
   end
 
-  def admin_title
+  def admin_title(title)
+    content_for(:title) { title.to_s }
+  end
+
+  def admin_page_title
     text = ""
-    text << AppConfig.site.name
     breadcrumbs.each do |title, link|
-      text << " - "
       text << t(title, scope: :breadcrumbs, default: title.to_s)
+      text << " - "
     end
+    text << AppConfig.site.name
     text
-  end
-
-  def title(title)
-    @title = title.to_s
-  end
-
-  def show_title
-    @title
   end
 
   def render_or_default(name)
