76961fd8190e0b54cbe4730b132d7cd3b9bb5eda
Marcio Trindade <marciotrindade@gmail.com>
2011-07-10 13:47:52 -0300

    to fix login page on post show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,8 @@ class PostsController < ApplicationController
   expose(:recent) { Post.active.recent }
   expose(:post)   { Post.active.find_by_permalink(params[:id]) }
   expose(:posts)  { Post.active.by_date(params[:year], params[:month], params[:day]) }
+  expose(:resource) { User.new }
+  expose(:resource_name) { :user }
 
   def index
   end
