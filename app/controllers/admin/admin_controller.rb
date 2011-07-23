bfb95e92592d5fd5768adeb7d5d7e9e0e0e06db9
Marcio Trindade <marciotrindade@gmail.com>
2011-07-22 23:25:31 -0300

    changed the render default to use the new way from rails 3.1

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -8,65 +8,26 @@ class Admin::AdminController < InheritedResources::Base
 
   layout "admin"
 
-  def index
-    index! do |format|
-      format.html { render_or_default(:index) }
-    end
-  end
-
-  def new
-    new! do |format|
-      format.html { render_or_default(:new) }
-    end
-  end
-
   def create
     create! do |success, failure|
-      success.html do
-        redirect_to(collection_path, notice: t("flash.create.success"))
-      end
-      success.xml { render xml: resource }
-      failure.html do
-        render_or_default(:new)
-      end
-    end
-  end
-
-  def edit
-    edit! do |format|
-      format.html { render_or_default(:edit) }
+      success.html { redirect_to(collection_path, notice: t("flash.create.success")) }
     end
   end
 
   def update
     update! do |success, failure|
-      success.html do
-        redirect_to(collection_path, notice: t("flash.update.success"))
-      end
-      failure.html do
-        render_or_default(:edit)
-      end
+      success.html { redirect_to(collection_path, notice: t("flash.update.success")) }
     end
   end
 
   def destroy
     destroy! do |success, failure|
-      success.html do
-        redirect_to(collection_path, notice: t("flash.destroy.success"))
-      end
-      failure.html do
-        redirect_to(collection_path, alert: t("flash.destroy.alert"))
-      end
+      success.html { redirect_to(collection_path, notice: t("flash.destroy.success")) }
+      failure.html { redirect_to(collection_path, alert: t("flash.destroy.alert")) }
     end
   end
 
   protected
-
-  def render_or_default(name, args = {})
-    render name, args
-  rescue ActionView::MissingTemplate
-    render args.merge(template: "../scaffolds/#{name}", prefix: '')
-  end
 
   def check_credentials
     authenticate_user!
