1289d1e539c05a2a3eed55b6ad35daa23ea45701
Marcio Trindade <marciotrindade@gmail.com>
2011-07-22 22:59:36 -0300

    fix breadcrumb inside the admin area

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,15 +1,10 @@
 class Admin::CommentsController < Admin::AdminController
 
-  add_breadcrumb :comments, "collection_path"
+  add_breadcrumb :comment, "collection_path"
   add_breadcrumb :create, '', only: [:new, :create]
   add_breadcrumb :edit, '', only: [:edit, :update]
   add_breadcrumb :show, '', only: [:show]
 
   actions :all, except: :show
-
-  private
-  def collection
-    @collection ||= Comment.approved
-  end
 
 end
