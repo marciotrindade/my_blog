e8a5dc99273984504363d1a77f36bae93d3830b7
Marcio Trindade <marciotrindade@gmail.com>
2011-11-05 20:31:39 -0200

    removed breadcrumb from application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,22 +15,6 @@ class ApplicationController < ActionController::Base
     rescue_from ActionController::UnknownAction,      with: :render_not_found
   end
 
-  protected
-
-  def self.add_breadcrumb name, url, options = {}
-    before_filter options do |controller|
-      controller.send(:add_breadcrumb, name, url)
-    end
-  end
-
-  def add_breadcrumb(name, link)
-    breadcrumbs << [name, link]
-  end
-
-  def breadcrumbs
-    @breadcrumbs ||= []
-  end
-
   private
 
   def render_not_found(exception)
