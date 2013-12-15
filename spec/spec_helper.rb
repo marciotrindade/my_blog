9bf082987f155426cf1ce22c8aa31636346b78cd
Marcio Trindade <marciotrindade@gmail.com>
2013-12-15 00:42:13 -0200

    changed spec_helper to be clear and simple

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,26 +5,30 @@ end
 
 ENV["RAILS_ENV"] ||= 'test'
 
-require "rails/application"
+
 require File.expand_path("../../config/environment", __FILE__)
-require "rspec/rails"
+require 'factory_girl_rails'
+require 'pry'
+require 'rspec/autorun'
+require 'rspec/rails'
+require 'shoulda-matchers'
 
 Rails.logger.level = 4
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  config.mock_with :rspec
-  config.infer_base_class_for_anonymous_controllers = false
-  config.use_transactional_fixtures = true
-
+  config.mock_with :rspec do |c|
+    # c.syntax = :expect
+  end
   config.expect_with :rspec do |c|
-    c.syntax = :expect
+    # c.syntax = :expect
   end
 
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
 
   config.include FactoryGirl::Syntax::Methods
-  config.include I18nMacros
 
   config.before :each do
     Rails.cache.clear
