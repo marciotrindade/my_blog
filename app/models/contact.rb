78e6727e3cabd35b36491533cd8a09e4b380ca59
Marcio Trindade <marciotrindade@gmail.com>
2013-12-15 00:39:53 -0200

    removed contact observer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,10 @@ class Contact < ActiveRecord::Base
 
   validates_presence_of :name, :email, :body
   validates :email, email_format: true
+  after_create :send_mail
+
+  def send_mail
+    ContactMailer.send_contact(self).deliver
+  end
 
 end
--- app/models/contact_observer.rb
+++
@@ -1,8 +0,0 @@
-class ContactObserver < ActiveRecord::Observer
-  observe Contact
-
-  def after_create(contact)
-    ContactMailer.send_contact(contact).deliver
-  end
-
-end
