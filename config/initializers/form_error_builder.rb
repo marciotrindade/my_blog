3d9ef6af26dc09f894032c8b3130c46537913caa
Marcio Trindade <marciotrindade@gmail.com>
2013-12-16 02:30:49 -0200

    created form_error_builder to inject 2 methods in form_builder

---
+++ config/initializers/form_error_builder.rb
@@ -0,0 +1,19 @@
+module ActionView
+  module Helpers
+    class FormBuilder
+
+      def error_class(method)
+        "has-error" if @object.errors[method].present?
+      end
+
+      def errors_for(method)
+        if @object.errors[method].present?
+          @template.content_tag :p, class: "help-block" do
+            @object.errors[method].join('<br>').html_safe
+          end
+        end
+      end
+
+    end
+  end
+end
