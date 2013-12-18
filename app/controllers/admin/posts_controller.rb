aefbd6ebfd1c54dacdfc049b3e8c8120427ae396
Marcio Trindade <marciotrindade@gmail.com>
2013-12-18 01:11:24 -0200

    [Admin::PostsController] added test

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,55 +1,57 @@
-class Admin::PostsController < Admin::BaseController
+module Admin
+  class PostsController < Admin::BaseController
 
-  before_action :set_post, only: [:edit, :update, :destroy]
+    before_action :set_post, only: [:edit, :update, :destroy]
 
-  # GET /posts
-  def index
-    @posts = Post.all
-  end
-
-  # GET /posts/new
-  def new
-    @post = Post.new
-  end
-
-  # GET /posts/1/edit
-  def edit
-  end
-
-  # POST /posts
-  def create
-    @post = Post.new(post_params)
-
-    if @post.save
-      redirect_to admin_posts_path, notice: 'Post was successfully created.'
-    else
-      render action: 'new'
+    # GET /posts
+    def index
+      @posts = Post.all
     end
-  end
 
-  # PATCH/PUT /posts/1
-  def update
-    if @post.update(post_params)
-      redirect_to admin_posts_path, notice: 'Post was successfully updated.'
-    else
-      render action: 'edit'
+    # GET /posts/new
+    def new
+      @post = Post.new
     end
+
+    # GET /posts/1/edit
+    def edit
+    end
+
+    # POST /posts
+    def create
+      @post = Post.new(post_params)
+
+      if @post.save
+        redirect_to admin_posts_path, notice: 'Post was successfully created.'
+      else
+        render action: 'new'
+      end
+    end
+
+    # PATCH/PUT /posts/1
+    def update
+      if @post.update(post_params)
+        redirect_to admin_posts_path, notice: 'Post was successfully updated.'
+      else
+        render action: 'edit'
+      end
+    end
+
+    # DELETE /posts/1
+    def destroy
+      @post.destroy
+      redirect_to admin_posts_path
+    end
+
+    private
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:name, :summary, :body, :keywords, :page_body, :active)
+    end
+
   end
-
-  # DELETE /posts/1
-  def destroy
-    @post.destroy
-    redirect_to admin_posts_path
-  end
-
-  private
-
-  def set_post
-    @post = Post.find(params[:id])
-  end
-
-  def post_params
-    params.require(:post).permit(:name, :summary, :body, :keywords, :page_body, :active)
-  end
-
 end
