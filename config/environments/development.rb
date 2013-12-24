ddd16a3a2f0a2e09efcd32527e263af44e2699c1
Marcio Trindade <marciotrindade@gmail.com>
2013-12-24 12:17:45 -0200

    changed logger to STDOUT in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Blog::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.logger = Logger.new(STDOUT)
 end
