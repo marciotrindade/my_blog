897a2cfd0d3ebe0e0b68c51f36099e8df31fcd1f
Marcio Trindade <marciotrindade@gmail.com>
2014-07-10 02:12:31 -0300

    removed config.logger to development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,4 @@ Blog::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  config.logger = Logger.new(STDOUT)
 end
