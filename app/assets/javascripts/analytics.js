d4d7423ef39e58e72800ecd6263f8b339934985a
Marcio Trindade <marciotrindade@gmail.com>
2014-07-14 02:35:36 -0300

    added google analytics

---
+++ app/assets/javascripts/analytics.js
@@ -0,0 +1,11 @@
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-1964688-4']);
+_gaq.push(['_trackPageview']);
+_gaq.push(['_trackPageLoadTime']);
+(function() {
+	var ga = document.createElement('script');
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	ga.setAttribute('async', 'true');
+	document.documentElement.firstChild.appendChild(ga);
+})();
